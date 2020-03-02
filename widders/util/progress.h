a5d97ca41239e0d4ffc0b2bd0c14efb91bd31ad7
widders <k@mad.cash>
2020-03-01 16:52:23 -0800

    make progress lib inline

--- widders/util/progress.h
+++ widders/util/progress.h
@@ -1,6 +1,7 @@
 #ifndef CC_WIDDERS_UTIL_PROGRESS_H_
 #define CC_WIDDERS_UTIL_PROGRESS_H_
 
+#include <algorithm>
 #include <cstdint>
 #include <functional>
 #include <utility>
@@ -10,12 +11,12 @@
 
 struct ProgressStats {
   // ETA to reach specified target.
-  absl::Duration eta(int64_t to_target_progress);
+  inline absl::Duration eta(int64_t to_target_progress);
 
-  int64_t progress;           // Progress reported.
-  absl::Duration elapsed;     // Total time elapsed.
-  double recent_update_rate;  // Progress per second since last update.
-  double harmonic_update_rate; // Harmonic mean of recent and average rate.
+  int64_t progress;             // Progress reported.
+  absl::Duration elapsed;       // Total time elapsed.
+  double recent_update_rate;    // Progress per second since last update.
+  double harmonic_update_rate;  // Harmonic mean of recent and average rate.
 };
 
 class Progress {
@@ -33,9 +34,9 @@ class Progress {
         last_output_time_(start_time_) {}
 
   // Update progress to output function if we should.
-  void update(int64_t progress);
+  inline void update(int64_t progress);
   // Time elapsed since init.
-  absl::Duration elapsed();
+  inline absl::Duration elapsed();
 
  private:
   const std::function<void(ProgressStats)> output_;
@@ -47,5 +48,38 @@ class Progress {
   int64_t next_check_progress_;
   absl::Time last_output_time_;
 };
+
+// -------------------------------------------------------------------------- //
+
+absl::Duration ProgressStats::eta(int64_t to_target_progress) {
+  return absl::Seconds(static_cast<double>(to_target_progress - progress) /
+                       harmonic_update_rate);
+}
+
+void Progress::update(int64_t progress) {
+  if (progress < next_check_progress_) return;
+
+  const absl::Time actual_now_time = absl::Now();
+  const absl::Time now_time =
+      std::max(actual_now_time, last_output_time_ + absl::Microseconds(10));
+  const absl::Duration elapsed = now_time - start_time_;
+  const absl::Duration since_last_output = now_time - last_output_time_;
+  const int64_t progress_made = progress - last_output_progress_;
+  const double recent_rate =
+      progress_made / absl::ToDoubleSeconds(since_last_output);
+  const double harmonic_rate =
+      2.0 /
+      (1 / recent_rate + absl::ToDoubleSeconds(elapsed) /
+                             static_cast<double>(progress - initial_progress_));
+  last_output_time_ = actual_now_time;
+  last_output_progress_ = progress;
+  next_check_progress_ =
+      progress +
+      static_cast<int64_t>(harmonic_rate *
+                           absl::ToDoubleSeconds(target_output_interval_));
+  output_({progress, elapsed, recent_rate, harmonic_rate});
+}
+
+absl::Duration Progress::elapsed() { return absl::Now() - start_time_; }
 
 #endif  // CC_WIDDERS_UTIL_PROGRESS_H_
