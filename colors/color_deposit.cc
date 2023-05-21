mumbleskates/color-deposit-cc

commit 9752212570185fcee5378415b1abd6e276740be9

updates

colors/color_deposit.cc
@@ -1,6 +1,6 @@
 #include <algorithm>
-#include <chrono>
 #include <cmath>
+#include <cstddef>
 #include <cstdint>
 #include <functional>
 #include <iostream>
@@ -13,21 +13,23 @@
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/flags/usage.h"
+#include "absl/random/distributions.h"
 #include "absl/random/random.h"
+#include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_split.h"
 #include "absl/types/variant.h"
-
 #include "colors/color_conversion.h"
 #include "third_party/lodepng/lodepng.h"
+#include "widders/container/kd_metrics.h"
 #include "widders/container/scapegoat_kd_map.h"
 #include "widders/util/progress.h"
 
 // Commandline flags
 ABSL_FLAG(bool, log_progress, false, "Log progress to stdout.");
 ABSL_FLAG(std::string, color_metric, "lab",
-          "The type of color metric to determine similar colors (one of "
-          "lab, luv, rgb, xyz, raw).");
+          "The type of color metric to determine similar colors (one of lab, "
+          "luv, xyz, rgb, srgb).");
 // TODO(widders): Hilbert... and zigzag?
 ABSL_FLAG(
     std::string, ordering, "shuffle",
@@ -41,8 +43,13 @@ ABSL_FLAG(
     "such components; for example, 'ordered:-r+b' would be the equivalent of "
     "all colors shuffled, then ordered by ascending blue value, then finally "
     "ordered by descending red value (with green left randomized).");
+ABSL_FLAG(
+    std::string, origin, "center",
+    "The origin in the image at which to start depositing. Possible values are "
+    "'center' for the center of the image, 'random' for a random location, "
+    "'x,y' with exact integer pixel coordinates, or 'x.xx%,y.yy%' for floating "
+    "point percentage coordinates relative to the size of the image.");
 // TODO(widders): dimensions: square, tall, wide
-// TODO(widders): origin location
 
 using std::cout;
 using std::endl;
@@ -51,12 +58,10 @@ using std::string;
 using std::vector;
 
 constexpr size_t kColorDims = 3;
-constexpr size_t kImageWidth = 1U << 12U;
-constexpr size_t kImageHeight = 1U << 12U;
-constexpr size_t kImageSize = kImageWidth * kImageHeight;
-constexpr int32_t kOriginX = kImageWidth / 2;
-constexpr int32_t kOriginY = kImageWidth / 2;
 constexpr uint32_t kEmpty = ~0U;
+
+constexpr int kAllColorImageWidth = 1U << 12U;
+constexpr int kAllColorImageHeight = 1U << 12U;
 
 struct Position {
   int x;
@@ -77,14 +82,43 @@ struct Position {
   }
 };
 
+using Canvas = vector<uint32_t>;
+
+struct ImageBuffer {
+  int width;
+  int height;
+  Canvas buffer;
+
+  static ImageBuffer OfSize(int width, int height) {
+    ImageBuffer result{
+        .width = width,
+        .height = height,
+    };
+    result.erase();
+    return result;
+  }
+
+  bool InBounds(Position pos) const {
+    return pos.x >= 0 && pos.y >= 0 && pos.x < static_cast<int>(width) &&
+           pos.y < static_cast<int>(height);
+  }
+
+  uint32_t& operator[](Position pos) { return buffer[pos.y * height + pos.x]; }
+
+  const uint32_t& operator[](Position pos) const {
+    return buffer[pos.y * height + pos.x];
+  }
+
+  size_t size() const { return buffer.size(); }
+  void erase() { buffer = Canvas(width * height, kEmpty); }
+};
+
 // Type for each color channel field.
 using Field = double;
-using RandomGenerator = std::mt19937_64;
 // Type for the frontier set.
 using ColorMap = widders::ScapegoatKdMap<kColorDims, Field, Position>;
 using Color = color::Color<Field>;
 using ColorPair = std::pair<uint32_t, Color>;
-using Canvas = vector<uint32_t>;
 using ColorMetric = std::function<Color(uint32_t int_srgb)>;
 
 // Offsets from any occupied pixel to any unoccupied pixel that is valid for
@@ -99,17 +133,6 @@ const std::pair<Position, Field> kSamples[] =       // NOLINT(cert-err58-cpp)
      {{+1, +0}, 1}, {{+1, +1}, 1 / std::sqrt(2)},   //
      {{+0, +1}, 1}, {{-1, +1}, 1 / std::sqrt(2)}};  //
 
-// Maps a Position to the index in our image vector where that pixel appears.
-inline size_t PositionToImageIndex(Position pos) {
-  return pos.y * kImageWidth + pos.x;
-}
-
-// Predicate for whether a Position is within the bounds of the image.
-inline bool PositionInBounds(Position pos) {
-  return pos.x >= 0 && pos.y >= 0 && pos.x < static_cast<int>(kImageWidth) &&
-         pos.y < static_cast<int>(kImageHeight);
-}
-
 // A pair of functions for generating and then (if necessary) reordering colors
 // before placement on the canvas.
 struct ColorOrder {
@@ -123,7 +146,7 @@ struct ColorOrder {
 // named by the color_metric flag.
 ColorMetric GetColorMetric(const string& metric_name) {
   return absl::flat_hash_map<string, ColorMetric>{
-      {"raw",
+      {"srgb",
        [](uint32_t int_srgb) {
          Color srgb = color::ExtractSRGB<Field>(int_srgb);
          return srgb;
@@ -169,7 +192,8 @@ absl::variant<string, ColorOrder> ParseOrdering(const string& ordering_name) {
     XYZ,
   };
   constexpr char kOrderClassError[] =
-      "Ordering channels must all come from the same class (rgb, luv, xyz).";
+      "Ordering channels must all come from the same colorspace (rgb, luv, "
+      "xyz).";
 
   struct OrderPart {
     int channel = 0;
@@ -319,7 +343,7 @@ absl::variant<string, ColorOrder> ParseOrdering(const string& ordering_name) {
               uint32_t result = 0;
               for (int i = 0; i < 3; ++i) {
                 const auto& part = ordering_parts[i];
-                uint32_t input_ch = (input & (0xff << (8U * i))) >> (8 * i);
+                uint32_t input_ch = (input & (0xffU << (8U * i))) >> (8U * i);
                 result |= (part.descending ? 255U - input_ch : input_ch)
                           << (8 * part.channel);
               }
@@ -370,6 +394,76 @@ absl::variant<string, ColorOrder> ParseOrdering(const string& ordering_name) {
   }
 }
 
+// Parses the origin location for the deposit process.
+absl::variant<string, Position> ParseOrigin(const string& origin_string,
+                                            const ImageBuffer& image,
+                                            absl::BitGen& rng) {
+  if (origin_string == "center") {
+    return Position{image.width / 2, image.height / 2};
+  }
+  if (origin_string == "random") {
+    return Position{
+        absl::Uniform(absl::IntervalClosedOpen, rng, 0, image.width),
+        absl::Uniform(absl::IntervalClosedOpen, rng, 0, image.height),
+    };
+  }
+  std::pair<string, string> coords =
+      absl::StrSplit(origin_string, absl::MaxSplits(absl::ByChar(','), 1));
+  std::string &x = coords.first, y = coords.second;
+  absl::StripAsciiWhitespace(&x);
+  absl::StripAsciiWhitespace(&y);
+  if (y.empty() || (absl::EndsWith(x, "%") != absl::EndsWith(y, "%"))) {
+    return "Expected 'center', 'x,y', or 'x.xx%,y.yy%' format for origin "
+           "coordinates.";
+  }
+  if (absl::EndsWith(x, "%")) {
+    // Remove trailing %
+    x.resize(x.size() - 1);
+    y.resize(y.size() - 1);
+    double x_percent;
+    double y_percent;
+    if (absl::SimpleAtod(x, &x_percent) && absl::SimpleAtod(y, &y_percent)) {
+      if (x_percent < 0 || x_percent > 100) {
+        return "Origin x percentage must be between 0% and 100%.";
+      }
+      if (y_percent < 0 || y_percent > 100) {
+        return "Origin y percentage must be between 0% and 100%.";
+      }
+      int x_exact = static_cast<int>(image.width * (x_percent / 100.0));
+      int y_exact = static_cast<int>(image.height * (y_percent / 100.0));
+      return Position{
+          std::min(image.width - 1, x_exact),
+          std::min(image.height - 1, y_exact),
+      };
+    } else {
+      return "Could not parse origin percentages.";
+    }
+  } else {
+    // Exact origin coordinates.
+    int x_exact;
+    int y_exact;
+    if (absl::SimpleAtoi(x, &x_exact) && absl::SimpleAtoi(y, &y_exact)) {
+      if (x_exact < 0 || x_exact >= image.width) {
+        return absl::StrCat("Origin x coordinate must be between 0 and ",
+                            image.width - 1);
+      }
+      if (y_exact < 0 || y_exact >= image.height) {
+        return absl::StrCat("Origin x coordinate must be between 0 and ",
+                            image.height - 1);
+      }
+      return Position{
+          .x = x_exact,
+          .y = y_exact,
+      };
+    } else {
+      return "Could not parse origin coordinates.";
+    }
+  }
+}
+
+// RNG instance
+static absl::BitGen rng;
+
 int main(int argc, char* argv[]) {
   absl::SetProgramUsageMessage("Color deposit: growing images like crystals.");
   const std::vector<char*> cmd_args = absl::ParseCommandLine(argc, argv);
@@ -387,7 +481,7 @@ int main(int argc, char* argv[]) {
   const auto& ordering = absl::get<ColorOrder>(maybe_ordering);
 
   vector<ColorPair> color_values;
-  color_values.reserve(kImageSize);
+  color_values.reserve(kAllColorImageWidth * kAllColorImageHeight);
 
   cout << "Generating colors..." << flush;
   for (uint32_t int_srgb = 0; int_srgb < 0x1000000U; ++int_srgb) {
@@ -395,9 +489,6 @@ int main(int argc, char* argv[]) {
     color_values.emplace_back<ColorPair>({mapped, {}});
   }
   cout << "done" << endl;
-
-  // RNG instance.
-  static absl::BitGen rng;
 
   if (ordering.should_shuffle) {
     cout << "Shuffling..." << flush;
@@ -423,27 +514,37 @@ int main(int argc, char* argv[]) {
   }
   cout << "done" << endl;
 
+  auto image = ImageBuffer::OfSize(kAllColorImageWidth, kAllColorImageHeight);
+  auto maybe_origin = ParseOrigin(absl::GetFlag(FLAGS_origin), image, rng);
+  if (absl::holds_alternative<string>(maybe_origin)) {
+    cout << "Error: " << absl::get<string>(maybe_origin) << endl;
+    return 1;
+  }
+  auto origin = absl::get<Position>(maybe_origin);
+
   cout << "Filling..." << flush;
-  Canvas image(kImageSize, kEmpty);
   auto frontier = ColorMap(1.4);
-  Progress progress([&frontier](ProgressStats stats) {
-    cout << stats.progress << " pixels done; "     // absolute progress
-         << stats.recent_update_rate << " px/s; "  // rate
-         << "ETA " << absl::Trunc(stats.eta(kImageSize), absl::Seconds(0.1))
-         << "; "                                          // ETA
-         << "frontier size " << frontier.size() << endl;  // frontier stats
-  });
+  ProgressOptions progress_options;
+  progress_options.output_function =
+      [&frontier, image_size = image.size()](ProgressStats stats) {
+        cout << stats.progress << " pixels done; "     // absolute progress
+             << stats.recent_update_rate << " px/s; "  // rate
+             << "ETA " << absl::Trunc(stats.eta(image_size), absl::Seconds(0.1))
+             << "; "                                          // ETA
+             << "frontier size " << frontier.size() << endl;  // frontier stats
+      };
+  Progress progress(progress_options);
 
   using Distance =
       widders::TiebreakingDistance<widders::EuclideanDistance<long double>,
-                                   widders::RandomTiebreak<absl::BitGen>>;
+                                   widders::RandomTiebreak<decltype(rng), rng>>;
 
   // Create initial frontier.
-  frontier.set({kOriginX, kOriginY}, Color());
+  frontier.set({origin.x, origin.y}, Color());
   // Repeatedly place a pixel in the image, updating the frontier set and all
   // affected color values of neighboring frontier pixels.
   const bool logging_enabled = absl::GetFlag(FLAGS_log_progress);
-  for (size_t i = 0; i < kImageSize; ++i) {
+  for (size_t i = 0; i < image.size(); ++i) {
     if (logging_enabled) progress.update(i);
     auto result = frontier.nearest<Distance>(color_values[i].second);
 #ifdef KD_TREE_DEBUG
@@ -454,12 +555,11 @@ int main(int argc, char* argv[]) {
 #endif
     const Position& pos = result.key;
     frontier.erase(pos);
-    image[PositionToImageIndex(pos)] = static_cast<uint32_t>(i);
+    image[pos] = static_cast<uint32_t>(i);
     for (const Position& f_delta : kFrontierOffsets) {
       Position frontier_pos = pos + f_delta;
       // Do nothing if this is not an empty frontier point.
-      if (!PositionInBounds(frontier_pos) ||
-          image[PositionToImageIndex(frontier_pos)] != kEmpty) {
+      if (!image.InBounds(frontier_pos) || image[frontier_pos] != kEmpty) {
         continue;
       }
       // Sum surrounding points for the new value of this frontier.
@@ -469,8 +569,8 @@ int main(int argc, char* argv[]) {
         const Position& s_delta = sample.first;
         const Field& sample_weight = sample.second;
         Position sample_pos = frontier_pos + s_delta;
-        if (!PositionInBounds(sample_pos)) continue;
-        const uint32_t sample_idx = image[PositionToImageIndex(sample_pos)];
+        if (!image.InBounds(sample_pos)) continue;
+        const uint32_t sample_idx = image[sample_pos];
         // Sum values of non-empty neighbors into new_mean.
         if (sample_idx == kEmpty) continue;
         total_weight += sample_weight;
@@ -489,11 +589,11 @@ int main(int argc, char* argv[]) {
   }
   absl::Duration elapsed = progress.elapsed();
   cout << "done; Total " << absl::Trunc(elapsed, absl::Seconds(0.1))
-       << "; Average " << kImageSize / absl::ToDoubleSeconds(elapsed) << " px/s"
-       << endl;
+       << "; Average " << image.size() / absl::ToDoubleSeconds(elapsed)
+       << " px/s" << endl;
 
   cout << "Unwrapping colors..." << flush;
-  for (auto& pixel : image) {
+  for (auto& pixel : image.buffer) {
     pixel = color::RenderABGR(color_values[pixel].first);
   }
   cout << "done" << endl;
@@ -504,9 +604,9 @@ int main(int argc, char* argv[]) {
   cout << "Encoding to file..." << flush;
   const string output_filename =
       cmd_args.size() <= 1 ? "output.png" : cmd_args[1];
-  auto error = lodepng::encode(output_filename,
-                               reinterpret_cast<unsigned char*>(image.data()),
-                               kImageWidth, kImageHeight);
+  auto error = lodepng::encode(
+      output_filename, reinterpret_cast<unsigned char*>(image.buffer.data()),
+      image.width, image.height);
   if (error) {
     cout << "encoder error " << error << ": " << lodepng_error_text(error)
          << endl;

widders/container/kd_metrics.h
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <limits>
+#include <type_traits>
 #include <utility>
 
 namespace widders {
@@ -219,11 +220,11 @@ struct TiebreakingDistance {
   Tiebreak tiebreak;
 };
 
-template <typename Rng>
+template <typename RngType, RngType& rng_callable>
 struct RandomTiebreak {
   RandomTiebreak() : val() {}
   template <typename... Args>
-  explicit RandomTiebreak(Args... args) : val(rng_instance()) {}
+  explicit RandomTiebreak(Args... args) : val(rng_callable()) {}
   RandomTiebreak(const RandomTiebreak& copy_from) = default;
   RandomTiebreak(RandomTiebreak&& move_from) noexcept = default;
   RandomTiebreak& operator=(const RandomTiebreak& assn) = default;
@@ -231,12 +232,8 @@ struct RandomTiebreak {
   bool operator==(const RandomTiebreak& other) { return val == other.val; }
   bool operator<(const RandomTiebreak& other) { return val < other.val; }
 
-  static Rng rng_instance;
-  typename Rng::result_type val;
+  decltype(rng_callable()) val;
 };
-
-template <typename Rng>
-Rng RandomTiebreak<Rng>::rng_instance = {};
 
 }  // namespace widders
 

widders/container/scapegoat_kd_map.h
@@ -4,7 +4,7 @@
 #include <array>
 #include <cassert>
 #include <cmath>
-#include <functional>
+#include <cstddef>
 #include <limits>
 #include <memory>
 #include <utility>
@@ -16,6 +16,8 @@
 #include "widders/container/kd_metrics.h"
 
 namespace widders {
+
+using ::std::size_t;
 
 template <typename NodeType>
 struct MedianOfMediansPolicy {
@@ -62,7 +64,7 @@ class ScapegoatKdMap {
   struct KdNode;
 
   // TODO(widders): maybe memoize
-  inline bool tree_is_balanced(size_t height, size_t node_count) const {
+  inline bool tree_is_balanced(uint32_t height, size_t node_count) const {
     // 1 + (implicit) floor of (log2 of ^ tree node count multiplied by the
     //                               height balance we maintain)
     return height <= 1 + static_cast<uint32_t>(std::log2f(node_count) *
@@ -240,7 +242,7 @@ class ScapegoatKdMap {
   size_t validate_node(const KdNode& node, const size_t dim, const Point& lower,
                        const Point& upper) const {
     // Check key reference is correct
-    assert(items_[node.key] == &node);
+    assert(items_.find(node.key)->second == &node);
     // Check val is inside the bounds
     for (size_t i = 0; i < dims; ++i) {
       assert(node.val[i] >= lower[i]);
@@ -276,6 +278,7 @@ class ScapegoatKdMap {
   // must never be null.
   // TODO(widders): consider queueing and deferring these updates?
   static void revise_stats(KdNode* node) {
+    assert(node);
     do {
       auto new_height = 1 + std::max(node->left ? node->left->height : 0,
                                      node->right ? node->right->height : 0);
@@ -293,56 +296,35 @@ class ScapegoatKdMap {
   // the tree looking for the smallest subtree that is unbalanced, then
   // rebuild it.
   void rebuild_one_ancestor(KdNode* tree, size_t dim) {
-    size_t node_count = count_nodes(tree);
+    assert(tree);
+    std::vector<std::unique_ptr<KdNode>> collection;
+    KdNode* current = tree;
     while (true) {
-      if (tree_is_balanced(tree->height, node_count)) {
+      KdNode* parent = current->parent;
+      std::unique_ptr<KdNode>* current_tree;
+      if (parent == nullptr) {
+        current_tree = &head_;
+      } else if (parent->left.get() == current) {
+        current_tree = &parent->left;
+      } else {
+        current_tree = &parent->right;
+      }
+      collect_nodes(std::move(*current_tree), &collection);
+      if (tree_is_balanced(current->height, collection.size()) &&
+          parent != nullptr) {
         // Subtree is sufficiently balanced; continue checking ancestors.
-        auto parent = tree->parent;
-        if (!parent) return;
-        // Add the nodes from the parent and sibling to node_count.
-        if (parent->left.get() == tree) {
-          node_count += 1 + count_nodes(parent->right.get());
-        } else {
-          node_count += 1 + count_nodes(parent->left.get());
-        }
-        tree = parent;
+        current = parent;
         // Keep dim in sync with tree's current level.
         dim = dim == 0 ? dims - 1 : dim - 1;
         continue;
       } else {
-        // Subtree is too tall: rebalance.
-        std::unique_ptr<KdNode>* tree_root;
-        if (tree->parent) {
-          KdNode* const parent = tree->parent;
-          if (parent->left.get() == tree) {
-            tree_root = &parent->left;
-          } else {
-            tree_root = &parent->right;
-          }
-        } else {
-          tree_root = &head_;
-        }
-        rebuild(*tree_root, node_count, dim);
+        *current_tree =
+            rebuild_recursive(dim, collection.begin(), collection.end());
+        (*current_tree)->parent = parent;
+        if (parent) revise_stats(parent);
         return;
       }
     }
-  }
-
-  static void rebuild(std::unique_ptr<KdNode>& tree_root, size_t node_count,
-                      size_t dim) {
-    KdNode* const parent = tree_root->parent;
-    std::vector<std::unique_ptr<KdNode>> nodes;
-    nodes.reserve(node_count);
-    collect_nodes(std::move(tree_root), &nodes);
-    tree_root = std::move(rebuild_recursive(dim, nodes.begin(), nodes.end()));
-    tree_root->parent = parent;
-    if (parent) revise_stats(parent);
-  }
-
-  static size_t count_nodes(KdNode* tree) {
-    return tree ? 1 + count_nodes(tree->left.get()) +
-                      count_nodes(tree->right.get())
-                : 0;
   }
 
   static void collect_nodes(std::unique_ptr<KdNode> node,
@@ -398,9 +380,9 @@ class ScapegoatKdMap {
   std::unique_ptr<KdNode> tree_pop_node(KdNode* const node) {
     KdNode* current = node;
     // Traverse down the deeper branch until we reach a leaf.
-    while (current->left || current->right) {
-      const auto left_depth = current->left ? current->left->height : 0;
-      if (current->right && current->right->height >= left_depth) {
+    while (current->height > 1) {
+      // Prefer to pick the rightmost deep leaf.
+      if (current->right && current->right->height == current->height - 1) {
         // Right subtree exists and is at least as deep as the left.
         current = current->right.get();
       } else {
@@ -443,15 +425,31 @@ class ScapegoatKdMap {
 
     // Check for tree balance.
     if (head_ && !tree_is_balanced(head_->height, size())) {
-      // Current is still the leaf node that we removed.
-      // Traverse upwards counting through dim so we can discover what dimension
-      // is the discriminant at popped_parent before rebalancing.
-      size_t dim = 0;
-      for (KdNode* n = popped_parent->parent; n; n = n->parent) {
-        dim = dim == dims - 1 ? 0 : dim + 1;
-      }
-      // dim is now the discriminant dim of popped_parent.
-      rebuild_one_ancestor(popped_parent, dim);
+      // If the tree is unbalanced after a removal, rebuild the whole tree.
+      std::vector<std::unique_ptr<KdNode>> collection;
+      collection.reserve(size());
+      collect_nodes(std::move(head_), &collection);
+      head_ = rebuild_recursive(0, collection.begin(), collection.end());
+      head_->parent = nullptr;
+
+      // Alternate method:
+
+      // // If the tree is unbalanced after a removal, rebuild some ancestor of
+      // // the deepest leaf in the tree.
+      // size_t dim = 0;
+      // current = head_.get();
+      // while (current->height > 1) {
+      //   // Prefer to rebalance from the leftmost deep leaf.
+      //   if (current->left && current->left->height == current->height - 1) {
+      //     current = current->left.get();
+      //   } else {
+      //     current = current->right.get();
+      //   }
+      //   dim = dim == dims - 1 ? 0 : dim + 1;
+      // }
+      // // current is now the deepest leaf in the tree and dim is its
+      // // discriminant.
+      // rebuild_one_ancestor(current, dim);
     }
     return std::move(popped);
   }

widders/util/progress.h
@@ -19,18 +19,22 @@ struct ProgressStats {
   double harmonic_update_rate;  // Harmonic mean of recent and average rate.
 };
 
+struct ProgressOptions {
+  std::function<void(ProgressStats)> output_function = nullptr;
+  absl::Duration target_output_interval = absl::Seconds(0.5);
+  int64_t initial_progress = 0;
+  int64_t first_output_progress = 1024;
+};
+
 class Progress {
  public:
-  explicit Progress(std::function<void(ProgressStats)> output,
-                    absl::Duration target_output_interval = absl::Seconds(0.5),
-                    int64_t initial_progress = 0,
-                    int64_t first_output_progress = 1024)
-      : output_(std::move(output)),
-        target_output_interval_(target_output_interval),
-        initial_progress_(initial_progress),
+  explicit Progress(ProgressOptions options = {})
+      : output_(std::move(options.output_function)),
+        target_output_interval_(options.target_output_interval),
+        initial_progress_(options.initial_progress),
         start_time_(absl::Now()),
-        last_output_progress_(initial_progress),
-        next_check_progress_(first_output_progress),
+        last_output_progress_(options.initial_progress),
+        next_check_progress_(options.first_output_progress),
         last_output_time_(start_time_) {}
 
   // Update progress to output function if we should.

commit 41e376470a6bc5f61b0550e356de51a845fdee7e

oops! fix "rgb" pre-sorting options that were broken in 9752212570185fcee5378415b1abd6e276740be9

colors/color_deposit.cc
@@ -358,7 +358,7 @@ absl::variant<string, ColorOrder> ParseOrdering(const string& ordering_name) {
         ColorMetric metric;
         switch (order_class) {
           case RGB: {
-            metric = GetColorMetric("raw");
+            metric = GetColorMetric("srgb");
             break;
           }
           case LUV: {